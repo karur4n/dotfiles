e6949aa8bc7a8d79d1de024fcd8aba8031f2c8cd
Kazuki Furukawa <6816398+karur4n@users.noreply.github.com>
2025-08-16 11:00:45 +0900

    update

--- private_dot_config/private_fish/conf.d/alias.fish
+++ private_dot_config/private_fish/conf.d/alias.fish
@@ -1,5 +1,10 @@
 alias c="cursor"
 alias cd="z"
+
+alias ls="eza -a --icons=always --color=always"
+alias ll="eza -a --icons=always --color=always -l"
+alias la="eza -a --icons=always --color=always -la"
+
 alias yolo="claude --dangerously-skip-permissions"
 alias vi="nvim"
 alias vim="nvim"
