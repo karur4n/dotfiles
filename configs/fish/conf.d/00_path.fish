82775ea6e85f1cae44546c2669c1ebf376439b44
kazuki furukawa <6816398+karur4n@users.noreply.github.com>
2026-02-18 09:59:08 +0900

    update

--- configs/fish/conf.d/00_path.fish
+++ configs/fish/conf.d/00_path.fish
@@ -1,5 +1,8 @@
-set --export PATH $HOME/bin $PATH
-set --export PATH $HOME/.local/bin $PATH
+fish_add_path $HOME/bin
+fish_add_path $HOME/.local/bin
+
+# obsidian
+fish_add_path /Applications/Obsidian.app/Contents/MacOS
 
 # Homebrew PATH (must load before other conf.d scripts)
 if test -x /opt/homebrew/bin/brew
