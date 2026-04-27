d628ebc92a128e0d9cf83c7e70fd87cd9a82c4fd
kazuki furukawa <6816398+karur4n@users.noreply.github.com>
2026-04-27 10:08:02 +0900

    update

--- configs/fish/functions/git_branch_fzf.fish
+++ configs/fish/functions/git_branch_fzf.fish
@@ -1,7 +1,24 @@
 function git_branch_fzf
-    set selected (git branch --sort=-committerdate --format='%(refname:short)' | fzf --layout reverse)
-    if test -n "$selected"
-        git switch $selected
-        commandline -f repaint
+    set -l selected (
+        git for-each-ref --sort=-committerdate \
+            --format='%(refname:short) %(worktreepath)' refs/heads/ \
+        | awk '{
+            if (NF >= 2) printf "%-40s ⇢ %s\n", $1, $2
+            else print $1
+        }' \
+        | fzf --layout reverse
+    )
+    if test -z "$selected"
+        return
     end
+
+    set -l branch (string split -m 1 ' ' -- $selected)[1]
+    set -l wt_path (git for-each-ref --format='%(worktreepath)' "refs/heads/$branch")
+
+    if test -n "$wt_path"
+        cd $wt_path
+    else
+        git switch $branch
+    end
+    commandline -f repaint
 end
