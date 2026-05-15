047fcea730c27da112d707dd635d0225a92a803f
kazuki furukawa <6816398+karur4n@users.noreply.github.com>
2026-05-15 09:50:00 +0900

    update

--- configs/nvim/lua/plugins/snacks.lua
+++ configs/nvim/lua/plugins/snacks.lua
@@ -13,6 +13,24 @@ return {
         explorer = {
           hidden = true,
           ignored = true,
+          actions = {
+            yank_rel_path = function(picker)
+              local item = picker:current()
+              if not item then
+                return
+              end
+              local rel = vim.fn.fnamemodify(Snacks.picker.util.path(item), ":.")
+              vim.fn.setreg("+", rel)
+              Snacks.notify.info("Yanked: " .. rel)
+            end,
+          },
+          win = {
+            list = {
+              keys = {
+                ["Y"] = "yank_rel_path",
+              },
+            },
+          },
         },
         grep = {
           hidden = true,
